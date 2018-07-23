a0c30c13dfbd4aab0a3c49fad6b6a241ae898cba
data-pup <kevinmartinucr@gmail.com>
2018-07-23 11:35:38 -0400

    Procedural macro works!

--- example/awoo_derive/src/lib.rs
+++ example/awoo_derive/src/lib.rs
@@ -1,7 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate awoo;
+extern crate proc_macro;
+extern crate proc_macro2;
+extern crate syn;
+#[macro_use]
+extern crate quote;
+
+#[proc_macro_derive(Awoo)]
+pub fn awoo_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    // Parse the token stream into a syntax tree.
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+
+    let name = ast.ident;
+
+    // Build the impl
+    let expanded = quote! {
+        impl Awoo for #name {
+            fn awoo() {
+                println!("I am a {}! Awoo!", stringify!(#name));
+            }
+        }
+    };
+
+    // Return the generated impl
+    expanded.into()
 }
--- example/run_example/src/main.rs
+++ example/run_example/src/main.rs
@@ -1,4 +1,6 @@
 extern crate awoo;
+#[macro_use]
+extern crate awoo_derive;
 
 use awoo::Awoo;
 
@@ -6,10 +8,18 @@ struct Puppo;
 
 impl Awoo for Puppo {
     fn awoo() {
-        println!("I'm a puppo! Awooooo!");
+        println!("I am a Puppo! Awoo!");
     }
 }
 
+#[derive(Awoo)]
+struct Doggo;
+
+#[derive(Awoo)]
+struct Friendo;
+
 fn main() {
     Puppo::awoo();
+    Doggo::awoo();
+    Friendo::awoo();
 }
