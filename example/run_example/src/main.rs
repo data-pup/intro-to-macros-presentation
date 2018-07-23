1b4adda13576c26cb2d02825a7a63068482ee979
data-pup <kevinmartinucr@gmail.com>
2018-07-23 10:41:47 -0400

    Awoo.

--- example/run_example/src/main.rs
+++ example/run_example/src/main.rs
@@ -1,3 +1,15 @@
+extern crate awoo;
+
+use awoo::Awoo;
+
+struct Puppo;
+
+impl Awoo for Puppo {
+    fn awoo() {
+        println!("I'm a puppo! Awooooo!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Puppo::awoo();
 }
